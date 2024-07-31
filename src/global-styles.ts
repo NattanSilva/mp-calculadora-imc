615ce004bd7b49d1e002892f3e0c4edae4fa065d
NattanSilva <natanxboy@gmail.com>
2024-07-31 17:14:30 -0300

    inicial configurations

---
+++ src/global-styles.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --background-color: #f4f4f4;
+    --text-one: #505050;
+    --text-two: #ababab;
+    --placeholder: #ABABAB;
+    --text-button: #FFFFFF;
+    --border-and-btn-bg: #E85B81;
+    --disabled-input: rgb(232 91 129 / 0.5);
+  }
+
+ *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    text-decoration: none;
+    list-style: none;
+    font-family: "Roboto", sans-serif;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    background-color: var(--background-color);
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+  }
+`;
